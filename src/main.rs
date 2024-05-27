29f56f4b633cf666ce1f0e9544a8ff6efef751dd
Adimac93 <adimac93@gmail.com>
2024-05-27 10:26:54 +0200

    📈 clean tracing

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,23 @@
+use axum_sqlx_template::{errors, setup::setup_globals};
 use axum_sqlx_template::routes::app;
 use axum_sqlx_template::setup::address;
 use axum_sqlx_template::state::AppState;
-use dotenvy::dotenv;
 use tokio::net::TcpListener;
 // use server::setup::address;
 // use server::{routes::app, state::AppState};
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::util::SubscriberInitExt;
 
 #[macro_use]
 pub extern crate tracing;
 
+
 #[tokio::main]
 async fn main() {
-    dotenv().ok();
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "server=debug,axum=trace".into()),
-        )
-        .with(tracing_subscriber::fmt::layer().without_time())
-        .init();
-
+    setup_globals();
+    
     let addr = address();
-    let listener = TcpListener::bind(addr).await.expect(&format!("Failed to bind to {addr}"));
+    let listener = TcpListener::bind(addr)
+        .await
+        .expect(&format!("Failed to bind to {addr}"));
 
     let app_state = AppState::new().await;
     info!("Constructed app state");
--- src/setup.rs
+++ src/setup.rs
@@ -3,6 +3,8 @@ use std::{
     net::{IpAddr, Ipv4Addr, SocketAddr},
 };
 
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, FmtSubscriber};
+
 const LOCAL_ADDR: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 3000);
 
 pub fn address() -> SocketAddr {
@@ -16,4 +18,42 @@ pub fn address() -> SocketAddr {
         .unwrap_or(LOCAL_ADDR);
 
     addr
+}
+
+pub fn setup_globals() {
+    dotenvy::dotenv().ok();
+    tracing_subscriber();
+
+}
+
+fn tracing_subscriber() {
+    tracing::subscriber::set_global_default(
+        FmtSubscriber::builder()
+            .pretty()
+            .without_time()
+            .with_env_filter(
+                EnvFilter::try_from_default_env()
+                    .unwrap_or(
+                        format!("{pkg}=trace", pkg = env!("CARGO_PKG_NAME")) // reads directly from Cargo.toml definition, RUST_LOG env will convert "-" to "_"
+                            .parse()
+                            .unwrap(),
+                    )
+                    .add_directive("axum::rejection=trace".parse().unwrap()),
+            ).finish(),
+    ).unwrap();
+
+    // same but different
+    // tracing_subscriber::registry()
+    // .with(
+    //     tracing_subscriber::EnvFilter::try_from_default_env()
+    //     .unwrap_or(
+    //         format!("{pkg}=trace", pkg = env!("CARGO_PKG_NAME")) // reads directly from Cargo.toml definition, RUST_LOG env will convert "-" to "_"
+    //             .parse()
+    //             .unwrap(),
+    //     )
+    //     .add_directive("axum::rejection=trace".parse().unwrap()),
+    // )
+    // .with(tracing_subscriber::fmt::layer().pretty()
+    // .without_time())
+    // .init();
 }
