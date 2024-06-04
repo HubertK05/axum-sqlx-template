44baccf7be9d685d9238df735044d3d72baaf89c
Adimac93 <adimac93@gmail.com>
2024-06-04 21:29:34 +0200

    🔊 propagate trace level logs

--- src/setup.rs
+++ src/setup.rs
@@ -2,29 +2,27 @@ use std::env;
 
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, FmtSubscriber};
 
-
 pub fn setup_globals() {
     dotenvy::dotenv().ok();
     tracing_subscriber();
 }
 
 fn tracing_subscriber() {
-    tracing::subscriber::set_global_default(
-        FmtSubscriber::builder()
-            .pretty()
-            .without_time()
-            .with_env_filter(
-                EnvFilter::try_from_default_env()
-                    .unwrap_or(
-                        format!("{pkg}=trace", pkg = env!("CARGO_PKG_NAME")) // reads directly from Cargo.toml definition, RUST_LOG env will convert "-" to "_"
-                            .parse()
-                            .unwrap(),
-                    )
-                    .add_directive("axum::rejection=trace".parse().unwrap()),
-            )
-            .finish(),
-    )
-    .unwrap();
+    let subscriber = FmtSubscriber::builder()
+        .pretty()
+        .without_time()
+        .with_env_filter(
+            EnvFilter::try_from_default_env()
+                .unwrap_or(
+                    format!("{pkg}=trace", pkg = env!("CARGO_PKG_NAME")) // reads directly from Cargo.toml definition, RUST_LOG env will convert "-" to "_"
+                        .parse()
+                        .unwrap(),
+                )
+                .add_directive("axum::rejection=trace".parse().unwrap())
+                .add_directive("sqlx=trace".parse().unwrap()),
+        )
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).unwrap();
 
     // same but different
     // tracing_subscriber::registry()
