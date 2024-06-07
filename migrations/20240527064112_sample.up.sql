009c9009769eced022c5b7c099aa359e82f56fe0
Adimac93 <adimac93@gmail.com>
2024-06-07 14:02:21 +0200

    🗃️ oauth2 mvp schema

--- migrations/20240527064112_sample.up.sql
+++ migrations/20240527064112_sample.up.sql
@@ -1,1 +1,18 @@
--- Add up migration script here
+CREATE TABLE users(
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    password TEXT
+);
+
+CREATE TYPE credential_provider AS ENUM(
+    'github'
+);
+
+CREATE TABLE federated_credentials(
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    provider credential_provider NOT NULL,
+    subject_id TEXT NOT NULL,
+    UNIQUE (subject_id, provider),
+    PRIMARY KEY (user_id, provider)
+);
+
+
