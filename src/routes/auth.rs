8fe600fb34db221ed30416e9675984918e639aa4
Adimac93 <adimac93@gmail.com>
2024-06-07 18:15:32 +0200

    🚧 standard authorization flow

--- src/routes/auth.rs
+++ src/routes/auth.rs
@@ -1,8 +1,165 @@
-use axum::Router;
+use crate::errors::AppError;
+use crate::errors::AppError::Expected;
+use crate::state::{AppState, RdPool};
 use crate::AppRouter;
+use axum::extract::{FromRef, FromRequestParts, State};
+use axum::http::request::Parts;
+use axum::http::StatusCode;
+use axum::response::{Html, IntoResponse};
+use axum::{async_trait, Json, RequestPartsExt, Router};
+use axum_extra::extract::CookieJar;
+use redis::{AsyncCommands, Expiry, RedisError};
+use serde::Deserialize;
+use sqlx::types::Uuid;
+use sqlx::PgPool;
+use std::str::FromStr;
+use axum::routing::{get, post};
+use axum_extra::extract::cookie::Cookie;
 
 mod oauth;
 
 pub fn router() -> AppRouter {
-    Router::new().nest("/oauth2", oauth::router())
+    Router::new()
+        .route("/register", post(register))
+        .route("/login", post(login))
+        .route("/session", get(session))
+        .nest("/oauth2", oauth::router())
+}
+
+#[derive(Deserialize)]
+struct RegistrationForm {
+    login: String,
+    password: String,
+}
+
+async fn register(
+    jar: CookieJar,
+    State(mut rds): State<RdPool>,
+    State(db): State<PgPool>,
+    Json(body): Json<RegistrationForm>,
+) -> crate::Result<impl IntoResponse> {
+    // TODO: check for session
+    let user_id = query!(
+        r#"
+    INSERT INTO users (login, password)
+    VALUES ($1, $2)
+    RETURNING id
+    "#,
+        &body.login,
+        &body.password
+    )
+    .fetch_one(&db)
+    .await?
+    .id; // TODO: handle duplicate login error
+
+    let session_id = Uuid::new_v4();
+    rds.set_ex(format!("session:{session_id}"), user_id, 60 * 10)
+        .await?;
+
+
+    Ok(SessionCookie::add(jar, &session_id))
+}
+
+#[derive(Deserialize)]
+struct LoginForm {
+    login: String,
+    password: String,
+}
+async fn login(
+    jar: CookieJar,
+    State(mut rds): State<RdPool>,
+    State(db): State<PgPool>,
+    Json(body): Json<LoginForm>,
+) -> crate::Result<impl IntoResponse> {
+    // TODO: check for session
+    let user_id = query!(
+        r#"
+    SELECT id FROM users
+    WHERE login = $1 AND password = $2
+    "#,
+        &body.login,
+        &body.password
+    )
+    .fetch_optional(&db)
+    .await?
+    .ok_or(AppError::exp(
+        StatusCode::UNAUTHORIZED,
+        "Invalid credentials",
+    ))?
+    .id;
+
+    let session_id = ClientSession::set(&mut rds, &user_id).await?;
+
+    Ok(SessionCookie::add(jar, &session_id))
+}
+
+async fn session(claims: Claims) -> crate::Result<impl IntoResponse> {
+    Ok(Html(format!("{}", claims.user_id)))
+}
+
+struct ClientSession;
+
+impl ClientSession {
+    async fn set(rds: &mut RdPool, user_id: &Uuid) -> Result<Uuid, RedisError> {
+        let session_id = Uuid::new_v4();
+        rds.set_ex(Self::key(&session_id), user_id, 60*10).await?;
+        Ok(session_id)
+    }
+
+    async fn get(rds: &mut RdPool, session_id: &Uuid) -> Result<Option<Uuid>, RedisError> {
+        let user_id: Option<Uuid> = rds.get_ex(Self::key(session_id), Expiry::EX(60 * 10))
+            .await?;
+        Ok(user_id)
+    }
+
+    fn key(session_id: &Uuid) -> String {
+        format!("session:{session_id}")
+    }
+}
+struct SessionCookie;
+
+impl SessionCookie {
+    fn add(jar: CookieJar, session_id: &Uuid) -> CookieJar {
+        jar.add(Cookie::build((SESSION_COOKIE_NAME, session_id.to_string())).build())
+    }
+}
+pub struct Claims {
+    pub session_id: Uuid,
+    pub user_id: Uuid,
+}
+
+const SESSION_COOKIE_NAME: &str = "session";
+
+#[async_trait]
+impl<S> FromRequestParts<S> for Claims
+where
+    S: Send + Sync,
+    RdPool: FromRef<S>,
+{
+    type Rejection = AppError;
+
+    async fn from_request_parts(parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
+        let jar = parts.extract::<CookieJar>().await.unwrap();
+
+        let Some(cookie) = jar.get(SESSION_COOKIE_NAME) else {
+            return Err(AppError::exp(
+                StatusCode::UNAUTHORIZED,
+                "Authentication required",
+            ));
+        };
+
+        let session_id = Uuid::from_str(cookie.value()).map_err(|_| {
+            AppError::exp(StatusCode::UNAUTHORIZED, "Session id must be a valid UUID")
+        })?;
+
+        let mut rds = RdPool::from_ref(state);
+        let Some(user_id): Option<Uuid> = ClientSession::get(&mut rds, &session_id).await? else {
+            return Err(AppError::exp(StatusCode::UNAUTHORIZED, "Invalid session"));
+        };
+
+        Ok(Self {
+            session_id,
+            user_id,
+        })
+    }
 }
--- src/routes/auth/oauth.rs
+++ src/routes/auth/oauth.rs
@@ -1,29 +1,32 @@
-use axum::{debug_handler, Json, Router};
+use crate::errors::AppError;
+use crate::AppRouter;
 use axum::extract::{Query, State};
 use axum::http::StatusCode;
 use axum::response::{Html, IntoResponse, Redirect};
 use axum::routing::get;
-use oauth2::{AuthorizationCode, CsrfToken, TokenResponse};
+use axum::{debug_handler, Json, Router};
 use oauth2::basic::BasicTokenResponse;
+use oauth2::{AuthorizationCode, CsrfToken, TokenResponse};
 use redis::{AsyncCommands, RedisWrite, ToRedisArgs};
 use serde::Deserialize;
 use serde_json::json;
-use sqlx::{PgExecutor, PgPool, Transaction};
 use sqlx::types::Uuid;
-use crate::AppRouter;
-use crate::errors::AppError;
+use sqlx::{PgExecutor, PgPool, Transaction};
 
 use crate::oauth::{AuthProvider, OAuthClient, OAuthClients, OAuthUser};
+use crate::routes::auth::ClientSession;
 use crate::state::{AppState, RdPool};
 
 pub fn router() -> AppRouter {
-    Router::new().route("/github/callback", get(handle_github_callback)).route("/github", get(issue_url))
+    Router::new()
+        .route("/github/callback", get(handle_github_callback))
+        .route("/github", get(issue_url))
 }
 
 #[derive(Deserialize)]
 struct OAuthQuery {
     code: AuthorizationCode,
-    state: CsrfToken
+    state: CsrfToken,
 }
 
 struct RedisKey;
@@ -35,10 +38,15 @@ impl RedisKey {
 }
 
 #[debug_handler(state = AppState)]
-async fn handle_github_callback(State(mut rds): State<RdPool>, State(db): State<PgPool>, State(oauth): State<OAuthClients>, Query(query): Query<OAuthQuery>) -> crate::Result<impl IntoResponse> {
+async fn handle_github_callback(
+    State(mut rds): State<RdPool>,
+    State(db): State<PgPool>,
+    State(oauth): State<OAuthClients>,
+    Query(query): Query<OAuthQuery>,
+) -> crate::Result<impl IntoResponse> {
     let is_matched_state: bool = rds.del(RedisKey::csrf(&query.state)).await?;
     if !is_matched_state {
-        return Err(AppError::exp(StatusCode::BAD_REQUEST, "Invalid CSRF state"))
+        return Err(AppError::exp(StatusCode::BAD_REQUEST, "Invalid CSRF state"));
     }
     trace!("Matching CSRF state for {}", query.state.secret());
 
@@ -46,53 +54,96 @@ async fn handle_github_callback(State(mut rds): State<RdPool>, State(db): State<
     let user = oauth.github.get_user(token.access_token()).await?;
     let subject_id = user.subject_id();
     let auth_provider = AuthProvider::Github;
-    if let Some(user_id) = select_user_id_from_federated_credentials(&db, &auth_provider, &subject_id).await? {
+    if let Some(user_id) =
+        select_user_id_from_federated_credentials(&db, &auth_provider, &subject_id).await?
+    {
+        ClientSession::set(&mut rds, &user_id).await?;
         trace!("{user_id} logged in with {auth_provider}");
         Ok(Html(format!("<h1>Authenticated with {auth_provider}</h1>")))
     } else {
-        let user_id = create_user_with_federated_credential(&db, &auth_provider, &subject_id).await?;
+        let user_id =
+            create_user_with_federated_credential(&db, &auth_provider, &subject_id).await?;
+        ClientSession::set(&mut rds, &user_id).await?;
         trace!("{user_id} registered with {auth_provider}");
         Ok(Html(format!("<h1>Authenticated with {auth_provider}</h1>")))
     }
 }
 
 #[debug_handler(state = AppState)]
-async fn issue_url(State(mut rds): State<RdPool>, State(oauth): State<OAuthClients>) -> crate::Result<impl IntoResponse> {
+async fn issue_url(
+    State(mut rds): State<RdPool>,
+    State(oauth): State<OAuthClients>,
+) -> crate::Result<impl IntoResponse> {
     let (url, csrf_token) = oauth.github.get_url_and_state();
     trace!("Issued federated authentication method url");
-    rds.set_ex(RedisKey::csrf(&csrf_token), AuthProvider::Github, 60*5 ).await?;
+    rds.set_ex(RedisKey::csrf(&csrf_token), AuthProvider::Github, 60 * 5)
+        .await?;
 
     Ok(Redirect::to(url.as_str()))
 }
 
-
-
-async fn create_user_with_federated_credential(db: &PgPool, auth_provider: &AuthProvider, subject_id: &str) -> sqlx::Result<Uuid>{
+async fn create_user_with_federated_credential(
+    db: &PgPool,
+    auth_provider: &AuthProvider,
+    subject_id: &str,
+) -> sqlx::Result<Uuid> {
     let mut tx = db.begin().await?;
     let user_id = insert_into_users(&mut *tx, None).await?;
     insert_into_federated_credentials(&mut *tx, &user_id, auth_provider, subject_id).await?;
     tx.commit().await?;
     Ok(user_id)
 }
-async fn select_user_id_from_federated_credentials(db: impl PgExecutor<'_>, provider: &AuthProvider, subject_id: &str) -> sqlx::Result<Option<Uuid>> {
-    Ok(query!(r#"
+async fn select_user_id_from_federated_credentials(
+    db: impl PgExecutor<'_>,
+    provider: &AuthProvider,
+    subject_id: &str,
+) -> sqlx::Result<Option<Uuid>> {
+    Ok(query!(
+        r#"
     SELECT user_id
     FROM federated_credentials
     WHERE provider = $1 AND subject_id = $2
-    "#, provider as _, subject_id).fetch_optional(db).await?.map(|r| r.user_id))
+    "#,
+        provider as _,
+        subject_id
+    )
+    .fetch_optional(db)
+    .await?
+    .map(|r| r.user_id))
 }
 
-async fn insert_into_federated_credentials(db: impl PgExecutor<'_>, user_id: &Uuid, provider: &AuthProvider, subject_id: &str) -> sqlx::Result<()> {
-    query!(r#"
+async fn insert_into_federated_credentials(
+    db: impl PgExecutor<'_>,
+    user_id: &Uuid,
+    provider: &AuthProvider,
+    subject_id: &str,
+) -> sqlx::Result<()> {
+    query!(
+        r#"
     INSERT INTO federated_credentials (user_id, provider, subject_id)
     VALUES ($1, $2, $3)
-    "#, user_id, provider as _, subject_id).execute(db).await?;
+    "#,
+        user_id,
+        provider as _,
+        subject_id
+    )
+    .execute(db)
+    .await?;
     Ok(())
 }
-async fn insert_into_users(db: impl PgExecutor<'_>, password: Option<String>) -> sqlx::Result<Uuid> {
-    Ok(query!(r#"
+async fn insert_into_users(
+    db: impl PgExecutor<'_>,
+    password: Option<String>,
+) -> sqlx::Result<Uuid> {
+    Ok(query!(
+        r#"
     INSERT INTO users (password)
     VALUES ($1)
     RETURNING id
-    "#, password).fetch_one(db).await?.id)
+    "#,
+        password
+    )
+    .fetch_one(db)
+    .await?
+    .id)
 }
