d3ca1fdfc19f21a2498db047ca11301edc782887
Adimac93 <adimac93@gmail.com>
2024-06-07 22:50:58 +0200

    ⚗ database error map util

--- src/errors.rs
+++ src/errors.rs
@@ -7,6 +7,7 @@ use oauth2::{HttpClientError, RequestTokenError};
 use redis::RedisError;
 use serde::Serialize;
 use sqlx::Error;
+use sqlx::error::ErrorKind;
 use thiserror::Error;
 use tracing::{debug, error};
 use typeshare::typeshare;
@@ -53,11 +54,80 @@ impl IntoResponse for AppError {
 }
 
 impl AppError {
-    pub fn exp(code: StatusCode, message: &str) -> Self {
+    pub fn exp(code: StatusCode, message: impl Into<String>) -> Self {
         Self::Expected {
             code,
-            message: message.to_string(),
+            message: message.into(),
         }
+    }
+}
+
+pub trait DbErrMap<T> {
+    fn map_db_err<O: FnOnce(DbErrMessage) -> DbErrMessage>(self, op: O) -> Result<T, DbErrMessage>;
+}
+
+impl<T> DbErrMap<T> for Result<T, sqlx::Error> {
+    fn map_db_err<O: FnOnce(DbErrMessage) -> DbErrMessage>(self, op: O) -> Result<T, DbErrMessage> {
+        match self {
+            Ok(t) => Ok(t),
+            Err(e) => Err(op(DbErrMessage::new(e))),
+        }
+    }
+}
+
+pub struct DbErrMessage {
+    pub err: AppError,
+    pub kind: ErrorKind,
+    pub name: String,
+}
+
+impl DbErrMessage {
+    pub fn new(err: sqlx::Error) -> Self {
+        let (kind, name) = if let Some(e) = err.as_database_error() {
+            (e.kind(), e.constraint().unwrap_or_default().to_string())
+        } else {
+            (ErrorKind::Other, "".to_string())
+        };
+
+        Self {
+            err: AppError::from(err),
+            kind,
+            name,
+        }
+    }
+
+    pub fn fk(mut self, code: StatusCode, message: &str) -> Self {
+        if self.kind == ErrorKind::ForeignKeyViolation {
+            self.err = AppError::exp(code, message);
+        }
+        self
+    }
+
+    pub fn unique(mut self, code: StatusCode, message: &str) -> Self {
+        if self.kind == ErrorKind::UniqueViolation {
+            self.err = AppError::exp(code, message);
+        }
+        self
+    }
+
+    pub fn not_null(mut self, code: StatusCode, message: &str) -> Self {
+        if self.kind == ErrorKind::NotNullViolation {
+            self.err = AppError::exp(code, message);
+        }
+        self
+    }
+
+    pub fn check(mut self, code: StatusCode, message: &str) -> Self {
+        if self.kind == ErrorKind::CheckViolation {
+            self.err = AppError::exp(code, message);
+        }
+        self
+    }
+}
+
+impl From<DbErrMessage> for AppError {
+    fn from(value: DbErrMessage) -> Self {
+        value.err
     }
 }
 
