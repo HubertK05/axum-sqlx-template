dc826c4e1f4b51ac0461894241a84d2c07bb3559
HubertK05 <hubert.klimowicz.hk@gmail.com>
2024-06-08 17:10:46 +0200

    :recycle: redesign token list

--- src/routes/auth/jwt.rs
+++ src/routes/auth/jwt.rs
@@ -11,16 +11,8 @@ use crate::{config::JwtConfiguration, errors::AppError, state::RdPool};
 const JWT_ACCESS_TOKEN_EXPIRY: Duration = Duration::minutes(5);
 const JWT_REFRESH_TOKEN_EXPIRY: Duration = Duration::days(7);
 
-fn access_whitelist(family_id: Uuid) -> String {
-    format!("token:{family_id}:access:whitelist")
-}
-
-fn unused_refresh_list(family_id: Uuid) -> String {
-    format!("token:{family_id}:refresh:unused")
-}
-
-fn used_refresh_list(family_id: Uuid) -> String {
-    format!("token:{family_id}:refresh:used")
+fn valid_token(family_id: Uuid) -> String {
+    format!("token:{family_id}:valid_token")
 }
 
 #[derive(Serialize, Deserialize, Clone, Copy)]
@@ -62,12 +54,14 @@ async fn init_token_family(
     user_id: Uuid,
 ) -> Result<(String, String), AppError> {
     let refresh_claims = Claims::family_root(user_id);
-    RefreshToken::add_to_unused(rds, refresh_claims).await?;
     let refresh_token = encode_jwt(refresh_claims, jwt_secrets.refresh_secret).context("Failed to encode JWT")?;
 
-    let access_token = AccessToken::create_from_refresh(rds, refresh_claims, jwt_secrets.access_secret).await?;
+    let access_claims = refresh_claims.new_member();
+    let access_token = encode_jwt(access_claims, jwt_secrets.access_secret).context("Failed to encode JWT")?;
 
-    Ok((refresh_token, access_token))
+    TokenFamily::set_valid(rds, refresh_claims).await?;
+
+    Ok((access_token, refresh_token))
 }
 
 async fn continue_token_family(
@@ -76,125 +70,49 @@ async fn continue_token_family(
     refresh_claims: Claims,
 ) -> Result<(String, String), AppError> {
     let new_refresh_claims = refresh_claims.new_member();
-    RefreshToken::add_to_unused(rds, new_refresh_claims).await?;
     let refresh_token = encode_jwt(new_refresh_claims, jwt_secrets.refresh_secret).context("Failed to encode JWT")?;
     
-    let access_token = AccessToken::create_from_refresh(rds, refresh_claims, jwt_secrets.access_secret).await?;
+    let access_claims = refresh_claims.new_member();
+    let access_token = encode_jwt(access_claims, jwt_secrets.access_secret).context("Failed to encode JWT")?;
 
-    Ok((refresh_token, access_token))
+    TokenFamily::set_valid(rds, refresh_claims).await?;
+
+    Ok((access_token, refresh_token))
 }
 
 async fn refresh(
-    mut rds: RdPool,
+    rds: &mut RdPool,
     refresh_claims: Claims,
     jwt_secrets: JwtConfiguration,
 ) -> Result<(String, String), AppError> {
-    if RefreshToken::is_in_unused_list(&mut rds, refresh_claims).await? {
-        RefreshToken::remove_from_unused(&mut rds, refresh_claims).await?;
-        RefreshToken::add_to_used(&mut rds, refresh_claims).await?;
-
-        continue_token_family(&mut rds, jwt_secrets, refresh_claims).await
-    } else if RefreshToken::is_in_used_list(&mut rds, refresh_claims).await? {
-        invalidate_family(&mut rds, refresh_claims.family).await?;
-        Err(AppError::exp(StatusCode::FORBIDDEN, "Refresh token reuse detected"))
+    if TokenFamily::is_valid_refresh_token(rds, refresh_claims).await? {
+        continue_token_family(rds, jwt_secrets, refresh_claims).await
     } else {
+        TokenFamily::invalidate(rds, refresh_claims.family).await?;
         Err(AppError::exp(StatusCode::FORBIDDEN, "Invalid refresh token"))
     }
 }
 
-struct AccessToken;
+struct TokenFamily;
 
-impl AccessToken {
-    async fn create_from_refresh(rds: &mut RdPool, refresh_claims: Claims, access_secret: String) -> Result<String, AppError> {
-        let access_claims = refresh_claims.new_member();
-        AccessToken::add_to_whitelist(rds, access_claims).await?;
-        let access_token = encode_jwt(access_claims, access_secret).context("Failed to encode JWT")?;
+impl TokenFamily {
+    async fn is_valid(rds: &mut RdPool, claims: Claims) -> RedisResult<bool> {
+        Ok(rds.exists(valid_token(claims.family)).await?)
+    }
+    
+    async fn is_valid_refresh_token(rds: &mut RdPool, claims: Claims) -> RedisResult<bool> {
+        let valid_token_id: Uuid = rds.get(valid_token(claims.family)).await?;
 
-        Ok(access_token)
+        Ok(valid_token_id == claims.jti)
     }
 
-    async fn add_to_whitelist(rds: &mut RdPool, claims: Claims) -> RedisResult<()> {
-        Ok(rds
-            .hset(
-                access_whitelist(claims.family),
-                claims.jti,
-                claims.exp,
-            )
-            .await?)
+    async fn set_valid(rds: &mut RdPool, claims: Claims) -> RedisResult<()> {
+        Ok(rds.set(valid_token(claims.family),claims.jti).await?)
     }
 
-    async fn is_in_whitelist(rds: &mut RdPool, claims: Claims) -> RedisResult<bool> {
-        let res: bool = rds
-            .hexists(
-                access_whitelist(claims.family),
-                claims.jti,
-            )
-            .await?;
-
-        Ok(res)
+    async fn invalidate(rds: &mut RdPool, family_id: Uuid) -> RedisResult<()> {
+        Ok(rds.del(valid_token(family_id)).await?)
     }
-}
-
-struct RefreshToken;
-
-impl RefreshToken {
-    async fn add_to_unused(rds: &mut RdPool, claims: Claims) -> RedisResult<()> {
-        Ok(rds
-            .hset(
-                unused_refresh_list(claims.family),
-                claims.jti,
-                claims.exp,
-            )
-            .await?)
-    }
-
-    async fn remove_from_unused(rds: &mut RdPool, claims: Claims) -> RedisResult<()> {
-        Ok(rds
-            .hdel(
-                unused_refresh_list(claims.family),
-                claims.jti,
-            )
-            .await?)
-    }
-
-    async fn add_to_used(rds: &mut RdPool, claims: Claims) -> RedisResult<()> {
-        Ok(rds
-            .hset(
-                used_refresh_list(claims.family),
-                claims.jti,
-                claims.exp,
-            )
-            .await?)
-    }
-
-    async fn is_in_unused_list(rds: &mut RdPool, claims: Claims) -> RedisResult<bool> {
-        let res: bool = rds
-            .hexists(
-                unused_refresh_list(claims.family),
-                claims.jti,
-            )
-            .await?;
-
-        Ok(res)
-    }
-
-    async fn is_in_used_list(rds: &mut RdPool, claims: Claims) -> RedisResult<bool> {
-        let res: bool = rds
-            .hexists(
-                used_refresh_list(claims.family),
-                claims.jti,
-            )
-            .await?;
-
-        Ok(res)
-    }
-}
-
-async fn invalidate_family(rds: &mut RdPool, family_id: Uuid) -> RedisResult<()> {
-    let _: () = rds.del(used_refresh_list(family_id)).await?;
-    let _: () = rds.del(unused_refresh_list(family_id)).await?;
-    let _: () = rds.del(access_whitelist(family_id)).await?;
-    Ok(())
 }
 
 fn encode_jwt(claims: Claims, secret: String) -> jsonwebtoken::errors::Result<String> {
@@ -217,39 +135,3 @@ fn decode_jwt<'a>(token: &str, secret: impl Into<&'a [u8]>) -> jsonwebtoken::err
 
     Ok(res.claims)
 }
-
-/*
-
-Access tokens:
-Tokens that are used for short-term, direct access to endpoints.
-They can be used multiple times, but we need to be aware of the access token whitelist.
-If the access token is not in the whitelist, reject.
-Logging out removes all access tokens of a given origin from the whitelist.
-Detected refresh token reuse has the same effect as logging out.
-
-Access token storage:
-In each token, we store its id, expiry and origin.
-In Redis, we can store up to 2 items if we want to use a hashmap. We store token id and expiry.
-And the hashmap is stored in the origin's Redis key. We are able to purge expired tokens
-and clear the entire hashmap stored in a given origin.
-
-Refresh tokens:
-The thing gets more complicated with refresh token.
-The idea is: we want to store refresh tokens in "unused" and "used" hashmaps, also in the refresh token origin's Redis key.
-A fresh refresh token is added to the "unused" list. When used, it is moved to the "used" list.
-When used again, all lists are deleted to invalidate all tokens of a given origin.
-Logging out has the same effect.
-Refresh tokens are stored alongside with expiry times. Tokens can be safely purged only from the "unused" list.
-
-The lists should be stored in Redis keys as hashmaps as follows:
-access token whitelist - token:{origin_id}:access:whitelist
-refresh token unused unused - token:{origin_id}:refresh:unused
-refresh token used list - token:{origin_id}:refresh:used
-
-*/
-
-
-
-
-
-
