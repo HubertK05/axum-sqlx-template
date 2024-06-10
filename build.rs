a1d1b36c9034fd341258875ffecd8d69b909a954
Adimac93 <adimac93@gmail.com>
2024-06-10 02:01:36 +0200

    🔨 compile protocol buffers in build step

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     // When building in docs.rs, we want to set SQLX_OFFLINE mode to true
     if std::env::var_os("DOCS_RS").is_some() {
         println!("cargo:rustc-env=SQLX_OFFLINE=true");
     }
+    tonic_build::compile_protos("proto/helloworld.proto")?;
+    Ok(())
 }
