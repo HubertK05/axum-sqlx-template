382b5582bd33201e34f115f9a1459cba113d02f7
HubertK05 <hubert.klimowicz.hk@gmail.com>
2024-06-13 11:36:52 +0200

    :sparkles: extend impls for DocExtractor and DocResponse

--- src/docutils.rs
+++ src/docutils.rs
@@ -1,6 +1,7 @@
 use std::{collections::{BTreeMap, HashMap}, fmt::Display, future::Future};
 
-use axum::{extract::{Path, Query}, handler::Handler, response::{Html, IntoResponse}, routing::MethodRouter, Json, Router};
+use axum::{extract::{Path, Query}, handler::Handler, response::{Html, IntoResponse, IntoResponseParts}, routing::MethodRouter, Json, Router};
+use axum_extra::extract::CookieJar;
 use regex::Regex;
 use utoipa::{openapi::{path::{Operation, Parameter, ParameterIn, PathItemBuilder}, request_body::RequestBody, Components, Content, Info, Object, ObjectBuilder, OneOf, OpenApi, PathItem, PathItemType, Paths, Ref, RefOr, Response, ResponseBuilder, Responses, Schema}, IntoParams, ToSchema};
 use axum::http::StatusCode;
@@ -10,10 +11,10 @@ pub trait DocHandler<T, S> {
     fn response_docs(&self) -> Option<(String, ContentType, RefOr<Schema>)>;
 }
 
-impl<F, S, Fut, R, E> DocHandler<((), ), S> for F
+impl<F, S, Fut, R> DocHandler<((), ), S> for F
 where
     F: Handler<((), ), S> + FnOnce() -> Fut,
-    Fut: Future<Output = Result<R, E>>,
+    Fut: Future<Output = R>,
     R: IntoResponse + DocResponse {
     fn extract_docs(&self) -> Vec<RequestPart> {
         vec![]
@@ -29,19 +30,19 @@ macro_rules! impl_doc_handler {
         $($ty:ident),*
     ) => {
         #[allow(non_snake_case, unused_mut)]
-        impl<F, M, $($ty),*, S, Fut, R, E> DocHandler<(M, $($ty),*), S> for F
+        impl<F, M, $($ty),*, S, Fut, R> DocHandler<(M, $($ty),*), S> for F
         where
             F: Handler<(M, $($ty),*), S> + FnOnce($($ty),*) -> Fut,
-            Fut: Future<Output = Result<(StatusCode, R), E>>,
+            Fut: Future<Output = R>,
             R: IntoResponse + DocResponse,
             $( $ty: DocExtractor, )*
         {
             fn extract_docs(&self) -> Vec<RequestPart> {
-                vec![
-                    $(
-                        $ty::doc_extractor(),
-                    )*
-                ]
+                let mut res = Vec::new();
+                $(
+                    res.extend($ty::doc_extractor());
+                )*
+                res
             }
 
             fn response_docs(&self) -> Option<(String, ContentType, RefOr<Schema>)> {
@@ -99,33 +100,40 @@ impl_doc_handler!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T
 impl_doc_handler!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16);
 
 pub trait DocExtractor {
-    fn doc_extractor() -> RequestPart;
+    fn doc_extractor() -> Option<RequestPart> {
+        None
+    }
 }
 
 impl<'a, T> DocExtractor for Path<T>
 where T: IntoParams {
-    fn doc_extractor() -> RequestPart {
-        RequestPart::Params(T::into_params(|| { Some(ParameterIn::Path) }))
+    fn doc_extractor() -> Option<RequestPart> {
+        Some(RequestPart::Params(T::into_params(|| { Some(ParameterIn::Path) })))
     }
 }
 
 impl<'a, T> DocExtractor for Query<T>
 where T: IntoParams {
-    fn doc_extractor() -> RequestPart {
-        RequestPart::Params(T::into_params(|| { Some(ParameterIn::Query) }))
+    fn doc_extractor() -> Option<RequestPart> {
+        Some(RequestPart::Params(T::into_params(|| { Some(ParameterIn::Query) })))
     }
 }
 
 impl<'a, T> DocExtractor for Json<T>
 where T: ToSchema<'a> {
-    fn doc_extractor() -> RequestPart {
+    fn doc_extractor() -> Option<RequestPart> {
         let (name, schema) = T::schema();
-        RequestPart::Schema(name.to_string(), ContentType::Json, schema)
+        Some(RequestPart::Schema(name.to_string(), ContentType::Json, schema))
     }
 }
 
+impl DocExtractor for axum_extra::extract::cookie::CookieJar {}
+impl<T> DocExtractor for axum::extract::State<T> {}
+
 pub trait DocResponse {
-    fn doc_response() -> Option<(String, ContentType, RefOr<Schema>)>;
+    fn doc_response() -> Option<(String, ContentType, RefOr<Schema>)> {
+        None
+    }
 }
 
 impl<'a, T> DocResponse for Json<T>
@@ -136,7 +144,7 @@ where T: ToSchema<'a> {
     }
 }
 
-impl DocResponse for Html<&str> {
+impl<T> DocResponse for Html<T> {
     fn doc_response() -> Option<(String, ContentType, RefOr<Schema>)> {
         let schema = Schema::Object(ObjectBuilder::new().schema_type(utoipa::openapi::SchemaType::String).build());
 
@@ -144,13 +152,53 @@ impl DocResponse for Html<&str> {
     }
 }
 
-impl DocResponse for () {
+impl DocResponse for () {}
+impl DocResponse for axum_extra::extract::cookie::CookieJar {}
+impl DocResponse for axum::response::Redirect {}
+
+impl<R> DocResponse for (R,)
+where
+    R: DocResponse {
     fn doc_response() -> Option<(String, ContentType, RefOr<Schema>)> {
-        None
+        R::doc_response()
     }
 }
 
-impl<T: DocResponse> DocResponse for (StatusCode, T) {
+macro_rules! impl_doc_response {
+    ($($ty:ident),*) => {
+        impl<$($ty),*, R> DocResponse for ($($ty),*, R)
+        where
+            $(
+                $ty: IntoResponseParts,
+            )*
+            R: IntoResponse + DocResponse {
+            fn doc_response() -> Option<(String, ContentType, RefOr<Schema>)> {
+                R::doc_response()
+            }
+        }
+    }
+}
+
+impl_doc_response!(T1);
+impl_doc_response!(T1, T2);
+impl_doc_response!(T1, T2, T3);
+impl_doc_response!(T1, T2, T3, T4);
+impl_doc_response!(T1, T2, T3, T4, T5);
+impl_doc_response!(T1, T2, T3, T4, T5, T6);
+impl_doc_response!(T1, T2, T3, T4, T5, T6, T7);
+impl_doc_response!(T1, T2, T3, T4, T5, T6, T7, T8);
+impl_doc_response!(T1, T2, T3, T4, T5, T6, T7, T8, T9);
+impl_doc_response!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10);
+impl_doc_response!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11);
+impl_doc_response!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12);
+impl_doc_response!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13);
+impl_doc_response!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14);
+impl_doc_response!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15);
+impl_doc_response!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16);
+
+impl<T, E> DocResponse for Result<T, E>
+where
+    T: DocResponse {
     fn doc_response() -> Option<(String, ContentType, RefOr<Schema>)> {
         T::doc_response()
     }
